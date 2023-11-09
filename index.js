b287d009619ccaf415c8d2c2ca79c7cfd7f66f60
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2023-11-09 17:25:31 +0300

    feat: add layout of burger menu and refactor decices width (Thu, Nov 09, 2023 05:25:31 PM)

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const burgerWrapper = document.querySelector(".burger__wrapper");
 const headerNavigation = document.querySelector(".header-nav");
+const body = document.querySelector("body");
 
+// if (!burgerWrapper) {
+//     headerNavigation.style.visibility = 'visible'
+// }
+// burgerWrapper.addEventListener('click', function (event) {
+//     // event.preventDefault();
+//     console.log(event.currentTarget)
+//     headerNavigation.classList.toggle('active');
+//     if (headerNavigation.classList.contains('active')) {
+//         headerNavigation.style.visibility = 'hidden';
+//     } else { headerNavigation.style.visibility = 'visible' };
+// })
